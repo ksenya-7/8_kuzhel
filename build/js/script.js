61f28e4a657d170362cd0dd395e7408067b2b14c
Ksu <ksenya-7@yandex.ru>
2021-01-26 18:52:01 +0300

    исправлена валидация

--- build/js/script.js
+++ build/js/script.js
@@ -62,10 +62,9 @@ var initPhoneMask = function () {
 
 initPhoneMask();
 
-// управление меню при навигации
+// управление меню при навигации и скролл
 var nav = document.querySelector('.site-navigation');
 var navToggle = document.querySelector('.main-header__toggle');
-var links = document.querySelectorAll('.site-navigation__link');
 
 if (navToggle) {
   navToggle.classList.remove('main-header__toggle--nojs');
@@ -81,14 +80,26 @@ if (nav) {
   });
 }
 
-if (links) {
-  links.forEach(function (element) {
-    element.addEventListener('click', function () {
-      nav.classList.remove('site-navigation--open');
-      navToggle.classList.remove('main-header__toggle--active');
+var moveTo = new window.MoveTo({
+  duration: 1000,
+  easing: 'easeOutQuart',
+});
+
+var initInnerLinks = function () {
+  var links = document.querySelectorAll('.site-navigation__link');
+
+  if (links.length) {
+    links.forEach(function (link) {
+      link.addEventListener('click', function () {
+        moveTo.registerTrigger(link);
+        nav.classList.remove('site-navigation--open');
+        navToggle.classList.remove('main-header__toggle--active');
+      });
     });
-  });
-}
+  }
+};
+
+initInnerLinks();
 
 // валидация полей формы
 var MIN_TEXT_LENGTH = 3;
@@ -103,72 +114,20 @@ if (textInput && telInput && buttonSubmit) {
     var text = textInput.value;
     var isLengthOfText = text.length < MIN_TEXT_LENGTH;
     var tel = telInput.value;
-    console.log(tel.length);
     var isLengthOfTel = tel.length !== TEL_LENGTH;
 
     if (isLengthOfText) {
       evt.preventDefault();
-      textInput.setCustomValidity('Минимальная длина имени 3 буквы.');
       textInput.style.border = '1px solid red';
     } else if (isLengthOfTel) {
       evt.preventDefault();
-      telInput.setCustomValidity('Введите номер телефона по образцу: +7(123) 456 78 90');
       telInput.style.border = '1px solid red';
     } else {
-      telInput.setCustomValidity('');
       telInput.style.border = '1px solid #c9ccd4';
-      textInput.setCustomValidity('');
       textInput.style.border = '1px solid #c9ccd4';
     }
 
     textInput.reportValidity();
     telInput.reportValidity();
   });
-}
-
-// if (textInput) {
-//   textInput.addEventListener('input', function (evt) {
-//     var text = textInput.value;
-//     var isLengthOfText = text.length < MIN_TEXT_LENGTH;
-//     evt.preventDefault();
-//     if (isLengthOfText) {
-//       textInput.setCustomValidity('Минимальная длина имени 3 символа.');
-//       textInput.style.border = '1px solid red';
-//     } else {
-//       textInput.setCustomValidity('');
-//       textInput.style.border = '1px solid #c9ccd4';
-//     }
-
-//     textInput.reportValidity();
-//   });
-// }
-
-// if (telInput) {
-//   telInput.addEventListener('input', function (evt) {
-//     var tel = telInput.value;
-//     var isLengthOfTel = tel.length !== TEL_LENGTH;
-//     evt.preventDefault();
-//     if (isLengthOfTel) {
-//       telInput.setCustomValidity('Количество цифр вместе с кодом – 10.');
-//       telInput.style.border = '1px solid red';
-//     } else {
-//       telInput.setCustomValidity('');
-//       telInput.style.border = '1px solid #c9ccd4';
-//     }
-
-//     telInput.reportValidity();
-//   });
-// }
-
-
-// скролл
-var moveTo = new window.MoveTo();
-var targets = document.querySelectorAll('.target-js');
-
-if (links && targets && links.length === targets.length) {
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function () {
-      moveTo.move(targets[i]);
-    });
-  }
 }
